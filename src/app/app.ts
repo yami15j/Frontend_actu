50594022610333f7c7aa97b89f67908c874fc03a
zyami15 <zyjumbo@sudamericano.edu.ec>
2025-12-18 22:11:44 -0500

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,39 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend');
+
+  textoMensaje = '';
+  mensajes: any[] = [];
+
+  // 👉 URL REAL DEL BACKEND EN RENDER
+  apiUrl = 'https://backend-actu.onrender.com/api/mensajes';
+
+  constructor(private http: HttpClient) {
+    this.cargarMensajes();
+  }
+
+  enviarMensaje() {
+    if (!this.textoMensaje.trim()) return;
+
+    this.http.post(this.apiUrl, { texto: this.textoMensaje })
+      .subscribe(() => {
+        this.textoMensaje = '';
+        this.cargarMensajes();
+      });
+  }
+
+  cargarMensajes() {
+    this.http.get<any[]>(this.apiUrl)
+      .subscribe(data => this.mensajes = data);
+  }
 }
